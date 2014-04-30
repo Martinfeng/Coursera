da9947058785dfb1b2d0319426762ee0893801ab
Martin <ericding1314@vip.qq.com>
2014-04-30 16:16:26 +0800

    machine learning

---
+++ 斯坦福Machine Learning/mlclass-ex2-005/plotData.m
@@ -0,0 +1,32 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+% find(y==1)是指y中y=1的元素的序号。
+% X（pos,1)是指X中第pos行，第1列所组成的向量。
+pos = find(y==1); neg = find(y == 0);
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
+
+
+
+
+
+
+
+
+% =========================================================================
+
+
+
+hold off;
+
+end
